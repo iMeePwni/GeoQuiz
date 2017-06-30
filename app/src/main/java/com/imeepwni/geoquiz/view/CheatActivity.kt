111f446ea8c3ac681b7db9dc6253f2e0018ef6b5
guofeng <imeepwni@outlook.com>
2017-06-30 08:58:07 +0800

    添加CheatActivity及相关资源文件
    Signed-off-by: guofeng <imeepwni@outlook.com>

---
+++ app/src/main/java/com/imeepwni/geoquiz/view/CheatActivity.kt
@@ -0,0 +1,13 @@
+package com.imeepwni.geoquiz.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.imeepwni.geoquiz.*
+
+class CheatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheat)
+    }
+}
